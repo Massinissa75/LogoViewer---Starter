2cc31707b5acf84187f74e1f7707918856dab74e
Massinissa75 <massinissa1theking@Gmail.com>
2019-12-04 14:12:21 +0100

    Functional application

--- LogoViewer/Controller/ViewController.swift
+++ LogoViewer/Controller/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textField: UITextField!
@@ -22,29 +22,40 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 
    func searchLogo(site: String) {
-       
-      if let site = textField.text  {
-        logoModel.gettingLogo(site: site) { (succes, logos) in
+      guard site != "" else {
+      return presentAlert(with: "Domain site is needed!")
+    }
+    logoModel.gettingLogo(site: site) { (success, Logos) in
       print("cool")
+      if success, let logos = Logos {
+        self.updateLogo(logo: Logos!)
+        
+      } else {
+        self.presentAlert(with: "Download error ")
       }
-    }
-  }
-
-  private func updateLogo(logo: Logo) {
-      imageView.image = UIImage(data: logo.imageData)
-  }
-
- //extension ViewController: UITextFieldDelegate {
-    
-   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-      textField.resignFirstResponder()
-      searchLogo(site: textField.text!)
-      return true
+      
     }
     
-
-    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
-        textField.resignFirstResponder()
-    }
+  }
+  private func updateLogo(logo: Logos) {
+    imageView.image = UIImage(data: logo.imageData)
+  }
+  
+  //extension ViewController: UITextFieldDelegate {
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    searchLogo(site: textField.text!)
+    return true
+  }
+  
+  @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+    textField.resignFirstResponder()
+  }
+  private func presentAlert(with message: String) {
+    let alertVC = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+    alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+    present(alertVC, animated: true, completion: nil)
+  }
 }
 
--- LogoViewer/Model/logoModel.swift
+++ LogoViewer/Model/logoModel.swift
@@ -14,31 +14,28 @@ class LogoModel {
   private var task: URLSessionDataTask?
   var domain: String?
   
-  func getLogo(from webSite: String) {
+  func gettingLogo(site:String, callback: @escaping (Bool, Logos?) -> Void) {
+    let completUrl = "\(apiUrl!)\(site)"
+    let request = URLRequest(url: URL(string: completUrl)!)
     let session = URLSession(configuration: .default)
-    let searchUrl = URL(string: LogoModel.apiUrlString + webSite)!
-    task = session.dataTask(with: searchUrl, completionHandler: { (data, response, error) in
-      print(response)
-      print(response as Any)
-    })
-    task?.resume()
-  }
-
-   func gettingLogo(site:String, callback: @escaping (Bool, Logos?) -> Void) {
-    let completUrl = "\(apiUrl))\(site)"
-    
-    let session = URLSession(configuration: .default)
-    task = session.dataTask(with: apiUrl!) { (data, response, error) in
+    task?.cancel()
+    task = session.dataTask(with: request) { (data, response, error) in
       DispatchQueue.main.async {
-        if let data = data, error == nil {
-          if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+        guard let data = data, error == nil else {
+          return callback(false, nil)
           
-            print(response)
-            callback(true, Logos(imageData: data))
-          }
         }
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+          return callback(false, nil)
+          
+        }
+        callback(true, Logos(imageData: data))
+        
       }
-      self.task?.resume()
+      
     }
+    task?.resume()
+    
   }
+  
 }
